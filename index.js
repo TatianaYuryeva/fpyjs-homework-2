c0ba4837162d337ead6c3c72a316d5cd628bc288
Tatiana Yuryeva <yuryevat@list.ru>
2022-11-26 17:18:25 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+while(true) {
+    let numberToGuess = Math.floor(Math.random() * 1000);
+
+    const numberFromUser = prompt('Введите целое число от 0 до 999. Выход: q');
+
+    if (numberFromUser === 'q') {
+        alert('Спасибо за игру!');
+        break;
+    } if (isNaN(numberFromUser)) {
+        alert('Вы ввели не число!');
+    } else if (numberToGuess > numberFromUser) {
+        alert('Вы ввели число меньше загаданного.');
+    } else if (numberToGuess < numberFromUser) {
+        alert('Вы ввели число больше загаданного.');
+    } else {
+        alert('Вы угадали!');
+    }
+}
